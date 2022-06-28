dd9904d10ab7af83e4c20c15872013b08e50c115
maciek <42122011+xkcm@users.noreply.github.com>
2022-06-28 06:53:25 +0200

    KP-798 [Maven] Remove method "Get Maven Version" (#5)

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ const kaholoPluginLibrary = require("@kaholo/plugin-library");
 const { execute } = require("./mvn-cli");
 
 module.exports = kaholoPluginLibrary.bootstrap({
-  testCli: () => execute({ command: "mvn --version" }),
   runCommand: execute,
 });
